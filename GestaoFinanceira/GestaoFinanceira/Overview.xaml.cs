d4d359009ad3f8b4fcfbfd6b39f2c7a64e3074f6
Lederback <renatosmachado2@gmail.com>
2022-10-30 14:23:00 -0300

    Initial Commit

---
+++ GestaoFinanceira/GestaoFinanceira/Overview.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestaoFinanceira
+{
+    /// <summary>
+    /// Lógica interna para Overview.xaml
+    /// </summary>
+    public partial class Overview : Window
+    {
+        public Overview()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Button).Foreground = Brushes.Black;
+        }
+
+        private void MenuButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FFFFFF");
+            Brush brush = new SolidColorBrush(color);
+            (sender as Button).Foreground = brush;
+        }
+    }
+}
