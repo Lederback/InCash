d1a84c54f4d0ba22b17c696978588fda2f01dbd3
Lederback <renatosmachado2@gmail.com>
2022-11-01 16:20:18 -0300

    API setup

---
+++ FinanceAPI/FinanceAPI/Controllers/FinanceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FinanceController : ControllerBase
+    {
+        private static List<Transaction> transaction = new List<Transaction>
+        {
+            new Transaction
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                Description = "Compra de açaí",
+                Value = 10.0M,
+                TypeTransaction = Transaction.TypeEnum.Lazer,
+            },
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Transaction>>> Get()
+        {
+            return Ok(transaction);
+        }
+    }
+}
---
+++ FinanceAPI/FinanceAPI/Transaction.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanceAPI
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date{ get; set; }
+        public decimal Value { get; set; }
+        public string? Description { get; set; }
+        public enum TypeEnum
+        {
+            Contas,
+            Lazer,
+            Alimentação,
+        }
+        public TypeEnum TypeTransaction { get; set; }
+    }
+}
